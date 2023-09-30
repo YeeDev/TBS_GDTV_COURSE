62a5878e0bd824e6a3eedeb169f3f56967b6cb87
Yee <yee.gamedev@gmail.com>
2023-09-30 10:39:48 -0600

    Section 3 - Lecture 28

--- Assets/Scripts/Testing.cs
+++ Assets/Scripts/Testing.cs
@@ -4,12 +4,15 @@ using UnityEngine;
 
 public class Testing : MonoBehaviour
 {
+    [SerializeField] private Transform gridDebugObjectPrefab;
+
     private GridSystem gridSystem;
 
     // Start is called before the first frame update
     void Start()
     {
-        gridSystem = new GridSystem(10, 10, 2f);  
+        gridSystem = new GridSystem(10, 10, 2f);
+        gridSystem.CreateDebugObjects(gridDebugObjectPrefab);
     }
 
     // Update is called once per frame
