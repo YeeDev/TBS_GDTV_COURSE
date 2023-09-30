90d51fd5f9575602339a0649e8b298b9fca208fe
Yee <yee.gamedev@gmail.com>
2023-09-30 10:58:19 -0600

    Section 3 - Lecture 29

--- Assets/Scripts/Testing.cs
+++ Assets/Scripts/Testing.cs
@@ -18,6 +18,6 @@ public class Testing : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Debug.Log(gridSystem.GetGridPosition(MouseWorld.GetPosition()));
+        
     }
 }
