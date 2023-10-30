6ed6d684be44218ee469f5452438251a3fd0120f
Yee <yee.gamedev@gmail.com>
2023-10-30 09:18:15 -0600

    Section 5 - Lecture 54

---
+++ Assets/Scripts/UnitRagdollSpawner.cs
@@ -0,0 +1,24 @@
+using System;
+using UnityEngine;
+
+public class UnitRagdollSpawner : MonoBehaviour
+{
+    [SerializeField] private Transform ragdollPrefab;
+    [SerializeField] private Transform rootBone;
+
+    private HealthSystem healthSystem;
+
+    private void Awake()
+    {
+        healthSystem = GetComponent<HealthSystem>();
+
+        healthSystem.OnDead += HealthSystem_OnDead;
+    }
+
+    private void HealthSystem_OnDead(object sender, EventArgs e)
+    {
+        Transform ragdoll = Instantiate(ragdollPrefab, transform.position, transform.rotation);
+        UnitRagdoll unitRagdoll = ragdoll.GetComponent<UnitRagdoll>();
+        unitRagdoll.Setup(rootBone);
+    }
+}
