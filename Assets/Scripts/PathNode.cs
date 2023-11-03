a0756bec38348f59d2493ae48c49c873ee1117fc
Yee <yee.gamedev@gmail.com>
2023-11-03 10:41:01 -0600

    Section 6 - Lecture 64

---
+++ Assets/Scripts/PathNode.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PathNode
+{
+    private GridPosition gridPosition;
+    private int gCost;
+    private int hCost;
+    private int fCost;
+
+    private PathNode cameFromPathNode;
+
+    public int GetGCost => gCost;
+    public int GetHCost => hCost;
+    public int GetFCost => fCost;
+
+    public PathNode(GridPosition gridPosition)
+    {
+        this.gridPosition = gridPosition; 
+    }
+
+    public override string ToString()
+    {
+        return gridPosition.ToString();
+    }
+}
---
+++ Assets/Scripts/PathfindingGridDebugObject.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class PathfindingGridDebugObject : GridDebugObject
+{
+    [SerializeField] private TextMeshPro gCostTexT;
+    [SerializeField] private TextMeshPro hCostTexT;
+    [SerializeField] private TextMeshPro fCostTexT;
+
+    private PathNode pathNode;
+
+    public override void SetGridObject(object gridObject)
+    {
+        pathNode = (PathNode)gridObject;
+        base.SetGridObject(gridObject);
+    }
+
+    protected override void Update()
+    {
+        base.Update();
+        gCostTexT.text = pathNode.GetGCost.ToString();
+        hCostTexT.text = pathNode.GetHCost.ToString();
+        fCostTexT.text = pathNode.GetFCost.ToString();
+    }
+}
